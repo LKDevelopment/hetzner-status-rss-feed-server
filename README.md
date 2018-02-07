c5c2c835f61bddf579282fb2f30503431318445d
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-02-07 13:06:45 +0100

    Make Page is little bit cleaner

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## Hetzner Status RSS Feed Server
 
-Dieser Server sorgt für die Statusmeldungen bei der Hetzner Cloud Mobile App.
+Dieser Server sorgt für die Push Notifications in der Hetzner Cloud Mobile App.
+
+Ebenso stellt er kostenfrei alle Statusmeldungen der Hetzner Online GmbH als JSON String zur Verfügung:
+https://hetzner-status.lkdev.co/api/hetzner-status
+
+Sollten Sie diesen Service selbst hosten wollen müssen Sie die Anwendung wie folgt installieren:
+```bash
+git clone https://github.com/LKDevelopment/hetzner-status-rss-feed-server
+cd hetzner-status-rss-feed-server
+nano .env // .env Datei bearbeiten und die Zugangsdaten zur Datenbank eintragen
+php artisan migrate
+```
+
+Push Notifications werden über den kostenfreien Dienst ["OneSignal"](https://onesignal.com/) versendet. 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,102 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Hetzner-Status JSON Service</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Hetzner-Status JSON Service
         </div>
-    </body>
+
+        <div class="links m-b-md">
+            <a href="https://github.com/LKDevelopment/hetzner-status-rss-feed-server">GitHub</a>
+        </div>
+        <div>
+            API
+            <pre>
+                <code class="bash">$ curl https://hetzner-status.lkdev.co/api/hetzner-status</code>
+            </pre>
+        </div>
+        <div class="mt-2">
+            Response
+            <pre>
+                <code class="json">{{ json_encode(App\StatusMeldung::onlyParents()->get(), JSON_PRETTY_PRINT) }}</code>
+            </pre>
+        </div>
+    </div>
+</div>
+<script>
+    $('pre code').each(function (i, block) {
+        hljs.highlightBlock(block);
+    });
+</script>
+</body>
 </html>
