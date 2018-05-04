7d128692485eda77fb08a65bc6f89dc8fce810c9
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-04 10:40:53 +0200

    Implement Tweetbot

---
+++ app/Console/Commands/TweetListener.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\Message;
+use App\StatusMeldung;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Spatie\LaravelTwitterStreamingApi\TwitterStreamingApi;
+use Thujohn\Twitter\Facades\Twitter;
+
+class TweetListener extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tweet';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        TwitterStreamingApi::publicStream()
+            ->whenHears('@HetzStatusBot', function (array $tweet) {
+                $messages = Message::where('title_en', 'LIKE', '%' . $tweet['text'] . '%')->where('created_at', '>', Carbon::now()->subDays(2)->startOfDay())->get();
+                if ($messages->count() == 0) {
+                    Twitter::postTweet(['status' => 'Hey ' . $tweet['user']['screen_name'] . ", i've found nothing for your request!", 'format' => 'json']);
+                } else {
+                    Twitter::postTweet(['status' => 'Hey ' . $tweet['user']['screen_name'] . ", i've found something: " . $messages->map(function ($m) { return $m->permalink_en; })->implode(' '), 'format' => 'json']);
+                }
+
+                echo "{$tweet['user']['screen_name']} tweeted {$tweet['text']}";
+            })
+            ->startListening();
+    }
+}
--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
          */
         ArandiLopez\Feed\Providers\FeedServiceProvider::class,
         Berkayk\OneSignal\OneSignalServiceProvider::class,
+        Thujohn\Twitter\TwitterServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -223,6 +224,7 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
+        'Twitter' => Thujohn\Twitter\Facades\Twitter::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
