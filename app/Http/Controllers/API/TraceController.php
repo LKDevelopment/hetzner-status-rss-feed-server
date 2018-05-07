e067c0ed721385ae3380fb1af10acdb8fe77c354
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-07 12:46:26 +0200

    Get only the 20 latest parents

--- app/Http/Controllers/API/TraceController.php
+++ app/Http/Controllers/API/TraceController.php
@@ -8,7 +8,6 @@
 
 namespace App\Http\Controllers\API;
 
-
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 use Cache;
@@ -17,23 +16,26 @@ use Illuminate\Validation\ValidationException;
 
 class TraceController extends Controller
 {
-
     public function get(Request $request, $ip)
     {
         $request['ip'] = $ip;
         $this->validate($request, [
             'ip' => ['required', 'ip', 'hetzner_ip'],
         ]);
+        var_dump(Cache::has($ip));
         if (Cache::has($ip)) {
             echo "Cache";
+
             return response()->json(Cache::get($ip));
         } else {
-            exec('traceroute ' . escapeshellarg($ip), $output);
+            exec('traceroute '.escapeshellarg($ip), $output);
             $hosts = [];
             foreach ($output as $index => $line) {
-                if ($index == 0) continue;
+                if ($index == 0) {
+                    continue;
+                }
                 $line_parts = explode(' ', ltrim($line));
-                if (!empty($line_parts) && $line_parts[2] != '*' && $line_parts[3] != '*') {
+                if (! empty($line_parts) && $line_parts[2] != '*' && $line_parts[3] != '*') {
                     $ip = str_replace(['(', ')'], '', $line_parts[3]);
                     $host = $line_parts[2];
                     if ($ip == $host) {
@@ -45,7 +47,6 @@ class TraceController extends Controller
             var_dump(Cache::put($ip, $hosts, now()->addHour()));
 
             return response()->json($hosts);
-
         }
     }
 }
