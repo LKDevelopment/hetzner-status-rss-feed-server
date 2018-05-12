a8bfa5e1cae63ae0aedf4a33e0539d90f22b2e27
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-12 16:28:41 +0200

    Get only the 20 latest parents

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
                 My Hetzner Backend
