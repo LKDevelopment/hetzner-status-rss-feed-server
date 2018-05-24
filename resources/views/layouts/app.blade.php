4144e75208939760f0156609a6c46c4cfb230fe9
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-24 15:37:40 +0200

    Add Description and Type to Device

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,5 +92,6 @@
         @yield('content')
     </main>
 </div>
+<script defer src="https://pro.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-d84LGg2pm9KhR4mCAs3N29GQ4OYNy+K+FBHX8WhimHpPm86c839++MDABegrZ3gn" crossorigin="anonymous"></script>
 </body>
 </html>
