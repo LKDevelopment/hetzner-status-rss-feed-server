c0933f8d1b8569062b37010b567fbf32e2cc656a
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-25 15:27:31 +0200

    Starts on Dashboard

--- resources/views/web/statics/device.blade.php
+++ resources/views/web/statics/device.blade.php
@@ -14,16 +14,28 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="float-left">App-Version</h3>
+                    </div>
+                    
+                    <div class="card-body">
+                        <canvas id="app_version" width="400" height="400"></canvas>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
 
 @push('js')
     <script>
-        let _data = [];
-        let _labels = [];
+
         let device_colors = ['#89BF64', '#F7D247', '#327BF6'];
         $.getJSON('/api/device/metrics2', function (data) {
+            let _data = [];
+            let _labels = [];
             $.each(data, function (index, val) {
                 _data.push(val.value);
                 _labels.push(val.os);
@@ -42,6 +54,38 @@
                 }
             });
         });
+        $.getJSON('/api/device/metrics3', function (data) {
+            let _data = [];
+            let _labels = [];
+            let _colors = [];
+            $.each(data, function (index, val) {
+                _data.push(val.value);
+                _labels.push(val.app_version);
+                _colors.push(getRandomColor());
+            });
+            // And for a doughnut chart
+            var myDoughnutChart = new Chart('app_version', {
+                type: 'doughnut',
+                data: {
+                    datasets: [{
+                        data: _data,
+                        backgroundColor: _colors,
+                    }],
+
+                    // These labels appear in the legend and in the tooltips when hovering different arcs
+                    labels: _labels
+                }
+            });
+        });
+
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
     
     </script>
 @endpush
