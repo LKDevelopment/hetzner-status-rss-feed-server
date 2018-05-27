6be0b858b3d5be2b12c9729cc439f2b4c38fd454
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-27 21:17:13 +0200

    Design

--- resources/views/web/statics/device.blade.php
+++ resources/views/web/statics/device.blade.php
@@ -150,7 +150,7 @@
                 let _colors = [];
                 $.each(data, function (index, val) {
                     _data.push({x: val.x, y: val.y});
-                    _labels.push(val.x + ' (' + val.y + ')');
+                    //_labels.push(val.x + ' (' + val.y + ')');
                     if (val.color == undefined) {
                         _colors.push(getRandomColor());
                     } else {
